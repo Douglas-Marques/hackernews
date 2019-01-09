45caaa52624bc29b7bff7965fd9ec3ffd45f393b
Douglas <douglas.silva@dbccompany.com.br>
2019-01-09 08:50:49 -0200

    feat(navbar): adicionar navbar casquinha

---
+++ app.js
@@ -0,0 +1,4 @@
+var app = angular.module("HackerNews", []);
+app.controller("NewsController", ['$scope', function($scope){
+
+}])
---
+++ navbar/navbar.js
@@ -0,0 +1,8 @@
+function NavbarController() {
+
+}
+
+angular.module('HackerNews').component('navbar', {
+  templateUrl: 'navbar/navbar.html',
+  controller: NavbarController
+});
